10c3f53e4a4b28d182b5b3d50a0b47cacba2c388
martipurpurpur <marqari1a@gmail.com>
2019-10-20 21:01:33 +0300

    "added handlers"

--- public/index.php
+++ public/index.php
@@ -7,7 +7,17 @@ use Slim\Factory\AppFactory;
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function ($request, $response) {
-    return $response->write("Welcome to Slim!");
+$app->get('/users', function ($request, $response) {
+    return $response->write("It's GET");
 });
+
+$app->post('/users', function ($request, $response) {
+    return $response->withStatus(302);
+});
+
+$app->get('/courses/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    return $response->write("Course id: {$id}");
+});
+
 $app->run();
