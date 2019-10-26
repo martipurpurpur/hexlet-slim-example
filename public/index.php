4e376a3143a5526254fc0cf843b8b01259e0e657
martipurpurpur <marqari1a@gmail.com>
2019-10-26 12:28:23 +0300

    "added simply form"

--- public/index.php
+++ public/index.php
@@ -4,7 +4,9 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 use DI\Container;
+use function Stringy\create as s;
 
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
 $container = new Container();
 $container->set('renderer', function () {
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
@@ -13,23 +15,19 @@ AppFactory::setContainer($container);
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/users', function ($request, $response) {
-    return $response->write("It's GET");
+$app->get('/', function ($request, $response) {
+   return $this->get('renderer')->render($response, "hello.phtml");
 });
-
-$app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
-});
-
-$app->get('/courses/{id}', function ($request, $response, array $args) {
-    $id = $args['id'];
-    return $response->write("Course id: {$id}");
-});
-
-$app->get('/users/{id}', function ($request, $response, $args) {
-    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
-    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+$app->get('/users', function ($request, $response) use ($users) {
+    $term = $request->getQueryParam('term');
+    $users = collect($users)->filter(function ($user) use ($term) {
+        return s($user)->startsWith($term, false);
+    })->all();
+    $params = [
+        'users' => $users,
+        'term' => $term
+    ];
+    return $this->get('renderer')->render($response, "users/show.phtml", $params);
 });
 
 $app->run();
-
